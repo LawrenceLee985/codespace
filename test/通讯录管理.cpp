438dc636d693c11d20a9d89913ba41282206d90a
LawrenceLee985 <1787728284@qq.com>
2023-05-23 18:21:49 +0800

    test

---
+++ test/通讯录管理.cpp
@@ -0,0 +1,19 @@
+//
+// Created by LarrenceLee on 2022/4/17.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//int* func(){
+//    int* p=new int(10);
+//    return p;
+//}
+//
+//int main(){
+//    int *p=func();
+//   cout<<*p<<endl;
+//    cout<<*p<<endl;
+//
+//    return 1;
+//}
