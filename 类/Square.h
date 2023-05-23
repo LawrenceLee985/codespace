edac6ffce793a74579ce4e4a3f4d5e0539b3216d
LawrenceLee985 <1787728284@qq.com>
2023-05-23 18:28:49 +0800

    类

---
+++ 类/Square.h
@@ -0,0 +1,61 @@
+//
+// Created by LarrenceLee on 2022/4/19.
+//
+/*
+#ifndef PROJECT01_SQUARE_H
+#define PROJECT01_SQUARE_H
+
+#include <iostream>
+using namespace std;
+
+class Square {
+
+private:
+    int length;
+    int width;
+    int height;
+
+public:
+    void setlength(int length){
+        this->length=length;
+    }
+
+    void setwidth(int width){
+        this->width=width;
+    }
+
+    void setheight(int height){
+        this->height=height;
+    }
+
+    int getlength(){
+        return this->length;
+    }
+
+    int getwidth(){
+        return this->width;
+    }
+
+    int getheight(){
+        return this->height;
+    }
+
+    int mainji(){
+        return 2*(length*width+length*height+width*height);
+    }
+
+    int tiji(){
+        return height*width*length;
+    }
+
+    void isSame(Square c2){
+        if(length==c2.length&&width==c2.width&&height==c2.height)
+            cout<<"两正方体相等"<<endl;
+        else
+            cout<<"两正方体不相等"<<endl;
+    }
+};
+
+
+#endif //PROJECT01_SQUARE_H
+*/
